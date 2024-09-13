2242b773ef25ade27f9751c1fd59f2f0abdad2ec
Daniel Ciborowski <dciborow@microsoft.com>
2024-09-12 23:02:32 -0400

    Discard changes to source/features/releases-tab.tsx

--- source/features/releases-tab.tsx
+++ source/features/releases-tab.tsx
@@ -122,3 +122,12 @@ void features.add(import.meta.url, {
 	],
 	init,
 });
+
+/*
+
+Test URLs:
+
+Releases: https://github.com/refined-github/refined-github
+Tags: https://github.com/python/cpython
+
+*/
